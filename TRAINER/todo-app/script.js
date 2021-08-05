2c6f924f5565f87e37be7bbcc7a1b1b766a8c3e3
Vadzim Naumchyk <vadzimn@ppedv.de>
2021-08-05 11:57:05 +0200

    m014 done

--- TRAINER/todo-app/script.js
+++ TRAINER/todo-app/script.js
@@ -3,9 +3,17 @@ const inpOkBtn = document.getElementById('okBtn');
 // Problem bei FctVariablen, sie müssen erstmal deklariert werden und dann dürfen sie genutzt werden.
 // Bei klassischen Funktionen kann Deklaration nach dem Aufruf stehen.
 okHandler = () => {
-    const parGreeter = document.getElementById('pGreeter');
+    const outputGreeter = document.getElementById('outpGreeter');
     const inpName = document.getElementById('inpName');
-    parGreeter.textContent = 'Hallo ' + inpName.value
+    if(inpName.value == '') {
+        // Bei Output kann sowohl 'value' als auch 'textContent' genutzt werden. 
+        // outputGreeter.textContent = 'Sie haben nichts eingegeben.'
+        outputGreeter.value = 'Sie haben nichts eingegeben.'
+        // value und textContent sind synchronisiert
+        console.log('outputGreeter.textContent :>> ', outputGreeter.textContent);
+    } else {
+        outputGreeter.textContent = 'Hallo ' + inpName.value;
+    }
 }
 
 // Option 1
